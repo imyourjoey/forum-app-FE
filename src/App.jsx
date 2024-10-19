da54705ee684cb4142f0a20cb3bb3442d6db0092
imyourjoey <joeylim016@gmail.com>
2024-10-19 17:04:59 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="default-padding">
+        <button className="btn btn-primary btn-sm">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
