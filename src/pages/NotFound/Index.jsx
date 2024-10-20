cc28f92f57c08a7c3a5615142f3bd0fbddc61838
imyourjoey <joeylim016@gmail.com>
2024-10-20 21:04:53 +0800

    add not found page

--- src/pages/NotFound/Index.jsx
+++ src/pages/NotFound/Index.jsx
@@ -1,5 +1,21 @@
+import NavBar from "../../components/Navbar";
+import IllusNotFound from "../../assets/Illustrations/IllusNotFound";
+
 function NotFound() {
-  return <div>not found</div>;
+  return (
+    <>
+      <div className="h-screen flex flex-col">
+        {/* Navbar at the top */}
+        <NavBar />
+
+        {/* Center the IllusNotFound */}
+        <div className="flex-grow flex flex-col  items-center justify-center">
+          <IllusNotFound width="50%" height="50%" />
+          <div className="text-4xl font-bold ogg">Nothing here...</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default NotFound;
